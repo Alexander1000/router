5301e9e563a5f0f78449eaa24c6402a6eaf5db84
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2016-11-20 16:25:35 +0300

    refactoring

--- tests/Router/RouterTest.php
+++ tests/Router/RouterTest.php
@@ -10,52 +10,164 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     const PATTERN_FIELD = 'uri';
 
     /**
-     * @return array
-     */
-    public function getDataRequest()
-    {
-        return require __DIR__ . '/../../share/fixtures/routes.php';
-    }
-
-    /**
      * Проверка построение объектов запроса
      *
-     * @dataProvider getDataRequest
+     * @dataProvider providerRoutes
      *
      * @param string $method
      * @param string $uri
      * @param array $params
      */
-    public function testResolve(string $method, string $uri, array $params)
+    public function testResolve_MethodRouteParams_Request(string $method, string $uri, array $params)
     {
         $router = new Router();
 
-        $router->setBasePath(__DIR__ . '/../../share')
+        $request = $router->setBasePath(__DIR__ . '/../../share')
             ->setPatternField(self::PATTERN_FIELD)
             ->setSchema('site')
             ->setMethod($method)
-            ->setUri($uri);
+            ->setUri($uri)
+            ->resolve();
 
-        try {
-            $response = $router->resolve();
-
-            if (empty($params)) {
-                $this->fail('Exception expected');
-            }
-
-            foreach ($params as $key => $value) {
-                $this->assertEquals($value, $response->{$key});
-            }
-        } catch (NotFound $e) {
-            if (!empty($params)) {
-                $this->fail('Exception not expected');
-            }
+        foreach ($params as $key => $value) {
+            $this->assertNotNull($request->{$key});
+            $this->assertEquals($value, $request->{$key});
         }
+    }
+
+    /**
+     * @return array
+     */
+    public function providerRoutes()
+    {
+        return [
+            'rootRoute' => [
+                'get',
+                '/',
+                [
+                    'param1' => 'test',
+                    'param2' => 'test2'
+                ]
+            ],
+            'subRouteRoot' => [
+                'get',
+                '/test',
+                [
+                    'uri' => '/',
+                    'action' => 'default',
+                    'controller' => 'AdminController'
+                ]
+            ],
+            'subRoute' => [
+                'get',
+                '/test/v2/kkk',
+                [
+                    'uri' => '/v2/kkk',
+                    'action' => 'kk',
+                    'controller' => 'AdminController'
+                ]
+            ],
+            'subRouteWithTwoAllowedMethods' => [
+                'get',
+                '/test/v3/dk',
+                [
+                    'uri' => '/v3/dk',
+                    'action' => 'dk',
+                    'controller' => 'AdminController'
+                ]
+            ],
+            'subRouteWithDigitPath' => [
+                'get',
+                '/sub/34',
+                [
+                    'uri' => '/34'
+                ]
+            ],
+            'subSubRouteRoot' => [
+                'get',
+                '/sub/sun',
+                [
+                    'uri' => '/',
+                    'action' => 'default',
+                    'controller' => 'AdminController'
+                ]
+            ],
+            'subSubRoute' => [
+                'get',
+                '/sub/sun/v2/kkk',
+                [
+                    'uri' => '/v2/kkk',
+                    'action' => 'kk',
+                    'controller' => 'AdminController'
+                ]
+            ],
+            'subSubRoutePath' => [
+                'get',
+                '/sub/sun/v3/dk',
+                [
+                    'uri' => '/v3/dk',
+                    'action' => 'dk',
+                    'controller' => 'AdminController'
+                ]
+            ],
+            'subSubRouteWithParameters' => [
+                'get',
+                '/sub/sun/v3/dk?test=1',
+                [
+                    'uri' => '/v3/dk',
+                    'action' => 'dk',
+                    'controller' => 'AdminController'
+                ]
+            ],
+            'routeWithPlaceholders' => [
+                'put',
+                '/test/user/123/edit',
+                [
+                    'action' => 'updateUser',
+                    'controller' => 'UserController',
+                    'params' => [
+                        'userId' => 123
+                    ]
+                ]
+            ],
+            'routeByMethod' => [
+                'put',
+                '/shop/shop-name',
+                [
+                    'action' => 'default',
+                    'controller' => 'AdminController',
+                    'params' => [
+                        'shopName' => 'shop-name'
+                    ]
+                ]
+            ],
+            'subRouteWithMethodAccess' => [
+                'put',
+                '/shop/shop-name/user/934/edit',
+                [
+                    'action' => 'updateUser',
+                    'controller' => 'UserController',
+                    'params' => [
+                        'shopName' => 'shop-name',
+                        'userId' => 934
+                    ]
+                ]
+            ],
+            'postRouteRoot' => [
+                'post',
+                '/',
+                [
+                    'param1' => 'test',
+                    'param2' => 'test2'
+                ]
+            ],
+        ];
     }
 
     /**
      * @dataProvider providerBadUrls
      * @expectedException \Router\Exception\NotFound
+     *
      * @param string $method
      * @param string $uri
      */
